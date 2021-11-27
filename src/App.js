bae6e71fac54bbb00f852dcd64727c6b155ba049
Parag Coimbatore <paragcoimbatore745@gmail.com>
2021-11-27 07:22:56 +0530

    Part1-Navbar with routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navbar />
+
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+
+        <Route exact path='/about' element={<About />}></Route>
+
+        <Route exact path='/services' element={<Services />}></Route>
+
+        <Route exact path='/signup' element={<Signup />}></Route>
+      </Routes>
+
+
     </div>
   );
 }
